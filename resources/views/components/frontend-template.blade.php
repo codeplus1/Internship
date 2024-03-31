bf99e833795cf7011a2398110db45b7c7019ac2c
sarojyadav88505 <sarojyadav88505@gmail.com>
2024-03-31 16:13:03 +0545

    external Css file removed

--- resources/views/components/frontend-template.blade.php
+++ resources/views/components/frontend-template.blade.php
@@ -20,7 +20,6 @@
     <link rel="stylesheet" href="/assets/css/custom.css">
     <link rel='shortcut icon' type='image/x-icon' href='/assets/img/favicon.ico' />
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-    <link rel="stylesheet" href="/assets/css/internship.css">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
